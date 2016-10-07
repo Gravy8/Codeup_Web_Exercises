d2986e3a65950fe4e8a76d9e5d241f74be2727d8
Brad Colton <bradcolton11@yahoo.com>
2016-10-07 14:19:51 -0500

    simplified the decrement

--- js/99_bottles.js
+++ js/99_bottles.js
@@ -6,11 +6,10 @@ while (numberOfBottles > 0) {
         nextNumberOfBottles = numberOfBottles - 1;
         console.log(numberOfBottles + " bottles of beer on the wall. " + numberOfBottles +
             " bottles of beer, take one down, pass it around. " + nextNumberOfBottles + " bottles of beer on the wall.");
-        numberOfBottles--;
     } else {
         console.log(numberOfBottles + " bottle of beer on the wall. " + numberOfBottles +
             " bottle of beer, take one down, pass it around... no more bottles of beer on the wall. " +
             "Someone should go to the store!");
-        numberOfBottles--;
     }
+        numberOfBottles--;
 }
