5211bbdaa7b0602130ded5886d39427a7a3da797
Brad Colton <bradcolton11@yahoo.com>
2016-10-11 15:59:49 -0500

    added extra exercise

--- js/iterating.js
+++ js/iterating.js
@@ -13,4 +13,15 @@
     for (var i = 0; i < names.length; i++) {
         console.log(names[i] + " is at position " + i + " in the names array.")
     }
+    var grades = [100, 80, 90, 80, 70];
+    var grade;
+    function average(grades) {
+        var totalOfGrades = 0;
+        for (i = 0; i < grades.length; i++) {
+            totalOfGrades += grades[i];
+        }
+        return totalOfGrades / i;
+    }
+    grade = average(grades);
+    console.log("You made a grade of " + grade + ".");
 })();
