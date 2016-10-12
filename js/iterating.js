24d7871c588bb9272dae48cd3045688f8039f3c4
Brad Colton <bradcolton11@yahoo.com>
2016-10-12 11:18:09 -0500

    added forEach exercise

--- js/iterating.js
+++ js/iterating.js
@@ -1,23 +1,35 @@
 (function(){
     "use strict";
 
-    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
     var names = ["Brad", "Nick", "Ryan", "Lloyd"];
-    // TODO: Create a log statement that will log the number of elements in the names array.
+
     console.log("There are " + names.length + " names in the names array.");
-    // TODO: Create log statements that will print each of the names array elements individually.
+
     console.log(names[0]);
     console.log(names[1]);
     console.log(names[2]);
     console.log(names[3]);
-    for (var i = 0; i < names.length; i++) {
+
+    names.forEach(function (element, index, array) {
+        console.log("The name at index " + index + " is " + element + ".");
+    });
+
+    names.forEach(function (element, index, array) {
+        if (element.indexOf("Brad") != -1) {
+            console.log("The name at index " + index + " is " + element + ".");
+        }
+    });
+
+
+
+    /*for (var i = 0; i < names.length; i++) {
         console.log(names[i] + " is at position " + i + " in the names array.")
-    }
+    }*/
     var grades = [100, 80, 90, 80, 70];
     var grade;
     function average(grades) {
         var totalOfGrades = 0;
-        for (i = 0; i < grades.length; i++) {
+        for (var i = 0; i < grades.length; i++) {
             totalOfGrades += grades[i];
         }
         return totalOfGrades / i;
