b43e9713e47574495f1e31e522db4d380e21cd00
Brad Colton <bradcolton11@yahoo.com>
2016-10-13 10:06:34 -0500

    added additional exercises

--- js/iterating.js
+++ js/iterating.js
@@ -1,7 +1,7 @@
 (function(){
     "use strict";
 
-    var names = ["Brad", "Nick", "Ryan", "Lloyd"];
+    /*var names = ["Brad", "Nick", "Ryan", "Lloyd"];
 
     console.log("There are " + names.length + " names in the names array.");
 
@@ -15,17 +15,17 @@
     });
 
     names.forEach(function (element, index, array) {
-        if (element.indexOf("Brad") != -1) {
+        if (element.indexOf("Ryan") != -1) {
             console.log("The name at index " + index + " is " + element + ".");
         }
-    });
+    });*/
 
 
 
     /*for (var i = 0; i < names.length; i++) {
         console.log(names[i] + " is at position " + i + " in the names array.")
     }*/
-    var grades = [100, 80, 90, 80, 70];
+    /*var grades = [100, 80, 90, 80, 70];
     var grade;
     function average(grades) {
         var totalOfGrades = 0;
@@ -35,5 +35,47 @@
         return totalOfGrades / i;
     }
     grade = average(grades);
-    console.log("You made a grade of " + grade + ".");
+    console.log("You made a grade of " + grade + ".");*/
+
+    var names = ["john", "paul", "ringo", "george", "yoko"];
+    var upperCaseNames = [];
+
+    function firstLetterToUppercase(array) {
+        var upperCaseWords = [];
+        array.forEach(function (word) {
+            upperCaseWords.push(word.replace(word[0], word[0].toUpperCase()));
+        });
+        return upperCaseWords;
+    }
+    upperCaseNames = firstLetterToUppercase(names);
+    console.log(upperCaseNames);
+
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var sumOfNumbers;
+
+    function productOfNumbers(array) {
+        var sum = 0;
+        array.forEach(function(number) {
+            sum += number;
+        });
+        return sum;
+    }
+    sumOfNumbers = productOfNumbers(numbers);
+    console.log(sumOfNumbers);
+
+    var listOfNames;
+    function arrayIntoList(array) {
+        var listString = "<ul>";
+        array.forEach(function(word) {
+            listString += "<li>" + word + "</li>";
+        });
+        listString += "</ul";
+        return listString;
+    }
+    listOfNames = arrayIntoList(names);
+    console.log(listOfNames);
+
+
+
+
 })();
